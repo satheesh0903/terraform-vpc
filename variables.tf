87169dba8627c5491409068a10f12db10564a6aa
unknown <bellamsatheeshkumar@gmail.com>
2025-01-30 10:44:32 +0530

    terraformbpc

--- variables.tf
+++ variables.tf
@@ -31,25 +31,25 @@ variable "igw_tags" {
   default = {}
 }
 
-variable "public_subnets_cidr" {
-  type = list
-  validation {
-    condition = length(var.public_subnets_cidr) == 2
-    error_message = "Please give 2 public valid subnet CIDR"
-  }
-}
+# variable "public_subnets_cidr" {
+#   type = list
+#   validation {
+#     condition = length(var.public_subnets_cidr) == 2
+#     error_message = "Please give 2 public valid subnet CIDR"
+#   }
+# }
 
-variable "public_subnets_tags" {
-  default = {}
-}
-variable "private_subnets_cidr" {
-  type = list
-  validation {
-    condition = length(var.private_subnets_cidr) == 2
-    error_message = "Please give 2 private valid subnet CIDR"
-  }
-}
+# variable "public_subnets_tags" {
+#   default = {}
+# }
+# variable "private_subnets_cidr" {
+#   type = list
+#   validation {
+#     condition = length(var.private_subnets_cidr) == 2
+#     error_message = "Please give 2 private valid subnet CIDR"
+#   }
+# }
 
-variable "private_subnets_tags" {
-  default = {}
-}
+# variable "private_subnets_tags" {
+#   default = {}
+# }
