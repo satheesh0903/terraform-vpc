403a72eca8f3ffd0a9b31dbf14f69f68cd8c5d81
unknown <bellamsatheeshkumar@gmail.com>
2025-01-30 11:02:12 +0530

    terraformbpc

---
+++ parameters.tf
@@ -0,0 +1,11 @@
+resource "aws_ssm_parameter" "vpc_id" {
+    name = "/${var.project_name}/${var.environment}/vpc_id"
+    type = "String"
+    value = module.roboshop.vpc_id
+}
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+    name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+    type = "StringList"
+    value = join(",", module.roboshop.public_subnet_ids)
+}
--- variables.tf
+++ variables.tf
@@ -30,18 +30,18 @@ variable "igw_tags" {
   type = map
   default = {}
 }
+variable "public_subnets_tags" {
+  default = {}
+}
 
-# variable "public_subnets_cidr" {
-#   type = list
-#   validation {
-#     condition = length(var.public_subnets_cidr) == 2
-#     error_message = "Please give 2 public valid subnet CIDR"
-#   }
-# }
+variable "public_subnets_cidr" {
+  type = list
+  validation {
+    condition = length(var.public_subnets_cidr) == 2
+    error_message = "Please give 2 public valid subnet CIDR"
+  }
+}
 
-# variable "public_subnets_tags" {
-#   default = {}
-# }
 # variable "private_subnets_cidr" {
 #   type = list
 #   validation {
